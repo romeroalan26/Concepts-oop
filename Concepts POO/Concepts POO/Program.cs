195bbd4f6d52e0a20f44d3c171cbc3eb458b745d
romeroalan26 <alan.266@hotmail.com>
2022-05-04 13:52:23 -0400

    Done for today

---
+++ Concepts POO/Concepts POO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsOOP;
+
+Console.WriteLine("OOP Concepts");
+Console.WriteLine("*************");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 5));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 12, 21));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
